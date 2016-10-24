c5dfd53375396a3b23b4756c436e4db368970176
Aaron Armstrong <aaron@openplus.ca>
2016-10-24 11:04:44 -0400

    adding if statments

--- templates/field--field-gallery-images.tpl.php
+++ templates/field--field-gallery-images.tpl.php
@@ -3,24 +3,31 @@
   $lang_code = $language->language;
 ?>
 <?php foreach ($items as $delta => $item) : ?>
-  <!-- Button trigger modal -->
   <?php
     module_load_include('inc', 'pathauto', 'pathauto');
     $divid = pathauto_cleanstring($item['#item']['field_file_image_title_text']['und'][0]['value']);
   ?>
-  <button type="button" class="toggle-button" data-toggle="modal" data-target="#<?php print $divid; ?>">
-    <p class="image"><?php print theme('image', array('path' => $item['#item']['uri'])); ?></p>
-  </button>
+  <?php if(isset($item['#item']['uri'])): ?>
+    <button type="button" class="toggle-button" data-toggle="modal" data-target="#<?php print $divid; ?>">
+      <p class="image"><?php print theme('image', array('path' => $item['#item']['uri'])); ?></p>
+    </button>
+  <?php endif; ?>
   <div class="modal fade" id="<?php print $divid; ?>" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title"><?php print $item['#item']['field_file_image_title_text']['und'][0]['value']; ?></h4>
+          <?php if(isset($item['#item']['field_file_image_title_text'])): ?>
+            <h4 class="modal-title"><?php print $item['#item']['field_file_image_title_text']['und'][0]['value']; ?></h4>
+          <?php endif; ?>
         </div>
         <div class="modal-body">
-          <p class="image"><?php print theme('image', array('path' => $item['#item']['uri'])); ?></p>
-          <p><?php print $item['#item']['field_ott_image_caption'][$lang_code][0]['value']; ?></p>
+          <?php if(isset($item['#item']['uri'])): ?>
+            <p class="image"><?php print theme('image', array('path' => $item['#item']['uri'])); ?></p>
+          <?php endif; ?>
+          <?php if(isset($item['#item']['field_ott_image_caption'])): ?>
+            <p><?php print $item['#item']['field_ott_image_caption'][$lang_code][0]['value']; ?></p>
+          <?php endif; ?>
         </div>
       </div>
     </div>
