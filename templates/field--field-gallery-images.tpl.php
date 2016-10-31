42d28e5de324e41eae627bd1e4627023e462143b
Aaron Armstrong <aaron@openplus.ca>
2016-10-31 13:41:46 -0400

    fix errors with issets

--- templates/field--field-gallery-images.tpl.php
+++ templates/field--field-gallery-images.tpl.php
@@ -5,21 +5,29 @@
 <?php foreach ($items as $delta => $item) : ?>
   <?php
     module_load_include('inc', 'pathauto', 'pathauto');
-    if(isset($item['#item']['field_file_image_title_text'])) {
+    if(isset($item['#item']['field_file_image_title_text']['und'][0]['value'])) {
       $divid = pathauto_cleanstring($item['#item']['field_file_image_title_text']['und'][0]['value']);
     }
   ?>
   <?php if(isset($item['#item']['uri'])): ?>
+    <?php if(isset($divid)): ?>
     <button type="button" class="toggle-button" data-toggle="modal" data-target="#<?php print $divid; ?>">
+    <?php else: ?>
+    <button type="button" class="toggle-button" data-toggle="modal">
       <p class="image"><?php print theme('image', array('path' => $item['#item']['uri'])); ?></p>
     </button>
+    <?php endif; ?>
   <?php endif; ?>
+  <?php if(isset($divid)): ?>
   <div class="modal fade" id="<?php print $divid; ?>" tabindex="-1" role="dialog">
+  <?php else: ?>
+  <div class="modal fade" tabindex="-1" role="dialog">
+  <?php endif; ?>
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <?php if(isset($item['#item']['field_file_image_title_text'])): ?>
+          <?php if(isset($item['#item']['field_file_image_title_text']['und'][0]['value'])): ?>
             <h4 class="modal-title"><?php print $item['#item']['field_file_image_title_text']['und'][0]['value']; ?></h4>
           <?php endif; ?>
         </div>
@@ -27,7 +35,7 @@
           <?php if(isset($item['#item']['uri'])): ?>
             <p class="image"><?php print theme('image', array('path' => $item['#item']['uri'])); ?></p>
           <?php endif; ?>
-          <?php if(isset($item['#item']['field_ottawa_image_caption'])): ?>
+          <?php if(isset($item['#item']['field_ottawa_image_caption']['und'][0]['value'])): ?>
             <p><?php print $item['#item']['field_ottawa_image_caption']['und'][0]['value']; ?></p>
           <?php endif; ?>
         </div>
